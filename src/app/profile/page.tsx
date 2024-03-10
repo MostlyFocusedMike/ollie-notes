e92f3d82ba98dc56daba974c0567ffc2e21cf62d
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2024-03-10 17:27:03 -0400

    [FEAT-2] Add basics for profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,18 +1,26 @@
 import SignInOrOutButton from '@/components/SignInOrOutButton';
+import Image from 'next/image';
 import { getServerSession } from 'next-auth';
 import { redirect } from 'next/navigation';
+import User from '@/models/User';
 
 // this is a protected route
 export default async function ProfilePage() {
   const session = await getServerSession();
+  if (!session?.user?.email) return redirect('/');
 
-  if (!session || !session.user) return redirect('/');
+  const profileUser = await User.findByEmail(session.user.email);
+  if (!profileUser) return redirect('/');
+  const { email, name, profilePic, bio, provider } = profileUser;
 
   return (
-    <div>
-      <h1>Profile</h1>
-      <p>Welcome {session.user.name}</p>
+    <>
+      <h1>{name}</h1>
+      <Image src={profilePic} alt={`${name}'s Avatar`} width={100} height={100} />
+      <p>{bio}</p>
+      <p>Email: {email}</p>
+      <p>Signed In Via: {provider}</p>
       <SignInOrOutButton />
-    </div>
+    </>
   );
 }
